a76993f36f11979fc6ffa0249bcf82b3c2f20c30
John Evans <lgastako@gmail.com>
2016-12-19 02:34:45 -0500

    Add elm loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
- module.exports = {
+module.exports = {
      entry: "./app.js",
      output: {
          path: ".",
          filename: "app.bundle.js"
      },
+     loaders: [{
+         test: /\.elm$/,
+         include: ["src"],
+         loader: `elm-webpack?cwd=${__dirname}/src`,
+         warn: true,
+         yes: true
+     }],
      devServer: {
          port: 2112,
          historyApiFallback: true
